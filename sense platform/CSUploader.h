962f6aae0ce6eef80968e914a6d374ba79c05a79
Joris Janssen <joris@sense-os.nl>
2015-06-18 12:54:04 +0200

    Refactored and optimized the upload of sensordata to be faster

--- sense platform/CSUploader.h
+++ sense platform/CSUploader.h
@@ -13,6 +13,14 @@
 /** Uploads data to CommonSense */
 @interface CSUploader : NSObject
 - (id) initWithStorage:(CSStorage*) theStorage andSender:(CSSender*) theSender;
+
+/**
+ Upload all data that has not been uploaded yet to CommonSense server. The function will fetch remote sensor IDs from the server and try to match them up with local sensors. If there is a local sensor that cannot be matched to a remote sensor it will create a new remote sensor for the sensordata. If it cannot fetch the remote sensor IDs it will return NO without uploading. 
+ 
+ @return Whether or not the upload was successful
+ */
 - (BOOL) upload;
+
+
 - (long long) lastUploadedRowId;
 @end
