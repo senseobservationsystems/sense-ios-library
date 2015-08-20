a679557450b548747cc6147d305a6b9b69322539
kanekin <tatsuya@sense-os.nl>
2015-08-20 17:59:29 +0200

    copied registration related methods from CSSender into the unitTest to avoid depending on CSSensePlatform. But now tests with getDevices methods are failing. Need to add a device on login or createSensor

--- Sense Library Tests/DSECommonSenseProxyTest.m
+++ Sense Library Tests/DSECommonSenseProxyTest.m
@@ -11,14 +11,37 @@
 #import "DSEHTTPRequestHelper.h"
 #import "DSECommonSenseProxy.h"
 #import "CSSensePlatform.h"
+#import "CSSensorStore.h"
+#import "NSData+GZIP.h"
+#import "NSString+MD5Hash.h"
+
 
 /* Some test values */
 static NSString* testAppKeyStaging = @"wRgE7HZvhDsRKaRm6YwC3ESpIqqtakeg";
 static NSString* newUserEmail_format = @"spam2+%f@sense-os.nl";
 static NSString* testPassword = @"darkr";
 
+static NSString* kUrlBaseURL = @"https://api.sense-os.nl";
+static NSString* kUrlBaseURLLive = @"https://api.sense-os.nl";
+static NSString* kUrlBaseURLStaging = @"http://api.staging.sense-os.nl";
+static NSString* kUrlAuthentication= @"https://auth-api.sense-os.nl/v1/login";
+static NSString* kUrlAuthenticationLive= @"https://auth-api.sense-os.nl/v1/login";
+static NSString* kUrlAuthenticationStaging= @"http://auth-api.staging.sense-os.nl/v1/login";
+
+static const NSString* kUrlLogin					= @"login";
+static const NSString* kUrlLogout                   = @"logout";
+static const NSString* kUrlSensorDevice             = @"device";
+static const NSString* kUrlSensors                  = @"sensors";
+static const NSString* kUrlUsers                    = @"users";
+static const NSString* kUrlUploadMultipleSensors    = @"sensors/data";
+static const NSString* kUrlData                     = @"data";
+static const NSString* kUrlDevices                  = @"devices";
+
+static const NSString* kUrlJsonSuffix               = @".json";
+
 
 @interface DSECommonSenseProxyTest : XCTestCase
+@property NSString* applicationKey;
 
 @end
 
@@ -47,11 +70,11 @@ static NSString* testPassword = @"darkr";
 
 - (void)testLoginWithValidUsernameAndPassword {
     
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-	NSError *error;
 	NSString *sessionID = [proxy loginUser:newUserEmail	andPassword:testPassword andError:&error];
 
 	XCTAssertNil(error, @"Error is not nil; an error must have occured");
@@ -93,12 +116,11 @@ static NSString* testPassword = @"darkr";
 
 - (void) testLogoutWithValidSessionID {
     
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-    //login
-    NSError *error;
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
     XCTAssertNotNil(sessionID, @"Session ID is nil; an error must have occured while logging in.");
     XCTAssertGreaterThan(sessionID.length, 0, @"Invalid session ID");
@@ -168,13 +190,12 @@ static NSString* testPassword = @"darkr";
 #pragma mark createSensor, getSensors, getDevices
 
 - (void) testCreateSensorAndGetSensor {
-
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-    NSError* error;
-    int expectedNumOfSensors = 1;
+    int expectedNumOfSensors = 0;
     
     //login
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
@@ -344,19 +365,18 @@ static NSString* testPassword = @"darkr";
 
 
 - (void) testCreateMultipleSensorsAndGetMultipleSensors {
-    
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-    NSError* error;
-    int expectedNumOfSensors = 1;
+    int expectedNumOfSensors = 0;
     
     //login
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
     XCTAssertNotNil(sessionID, @"Session ID is nil; an error must have occured while logging in.");
     
-    [CSSensePlatform flushDataAndBlock];
+    [self flushDataAndBlock];
     
     //make sure that there is no sensor yet
     NSArray *sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
@@ -396,19 +416,18 @@ static NSString* testPassword = @"darkr";
 }
 
 - (void) testCreateMultipleSensorsAndGetDevices {
-    
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
-    
-    NSError* error;
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
+
     int expectedNumOfDevices = 1;
     
     //login
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
     XCTAssertNotNil(sessionID, @"Session ID is nil; an error must have occured while logging in.");
 
-    [CSSensePlatform flushDataAndBlock];
+    [self flushDataAndBlock];
     
     //make sure that there is no sensor yet
     NSArray *sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
@@ -498,15 +517,21 @@ static NSString* testPassword = @"darkr";
 
 -(void) testAddSensorToDeviceWithDeviceTypeAndUUID {
     
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-    NSError* error;
-    int expectedNumOfSensors = 1;
+    int expectedNumOfSensors = 0;
     //login
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
     XCTAssertNotNil(sessionID, @"Session ID is nil; an error must have occured while logging in.");
+    
+    [self flushDataAndBlock];
+    
+    //make sure that there is no sensor yet
+    NSArray *sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
+    XCTAssertEqual(sensors.count, expectedNumOfSensors, "The number of sensors is not 1.");
     
     //createSensor and get sensorId
     NSString* name = @"test";
@@ -530,7 +555,18 @@ static NSString* testPassword = @"darkr";
     
     //get list of sensors and count
     error = nil;
-    NSArray *sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
+    sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
+    XCTAssertNil(error, @"Error is not nil; an error must have occured");
+    XCTAssertEqual(sensors.count, expectedNumOfSensors, @"Unexpected number of sensors" );
+    
+    name = @"test1";
+    displayName = @"test1";
+    [proxy createSensorWithName:name andDisplayName:displayName andDeviceType:deviceType andDataType:dataType andDataStructure:dataStructure andSessionID:sessionID andError:&error];
+    expectedNumOfSensors++;
+    
+    //get list of sensors and count
+    error = nil;
+    sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
     XCTAssertNil(error, @"Error is not nil; an error must have occured");
     XCTAssertEqual(sensors.count, expectedNumOfSensors, @"Unexpected number of sensors" );
     
@@ -556,11 +592,11 @@ static NSString* testPassword = @"darkr";
 
 -(void) testPostAndGetData {
     
+    NSError* error;
     NSTimeInterval registrationTimestamp = [[NSDate date] timeIntervalSince1970];
     NSString* newUserEmail = [NSString stringWithFormat: newUserEmail_format, registrationTimestamp];
-    [CSSensePlatform registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail];
+    [self registerUser:newUserEmail withPassword:testPassword withEmail:newUserEmail error:&error];
     
-    NSError* error;
     
     //login
     NSString *sessionID = [proxy loginUser:newUserEmail andPassword:testPassword andError:&error];
@@ -647,6 +683,135 @@ static NSString* testPassword = @"darkr";
     NSArray *sensors = [proxy getSensorsWithSessionID: sessionID andError: &error];
     XCTAssertNil(error, @"Error is not nil; an error must have occured");
     XCTAssertEqual(sensors.count, expectedNumOfSensors, @"Unexpected number of sensors" );
+}
+
+- (BOOL) registerUser:(NSString*) user withPassword:(NSString*) pass withEmail:(NSString*) email error:(NSString**) error
+{
+    //prepare post
+    NSMutableDictionary* userPost = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                     user, @"username",
+                                     [pass MD5Hash], @"password",
+                                     nil];
+    if (email)
+        [userPost setValue:email forKey:@"email"];
+    else
+        [userPost setValue:user forKey:@"email"];
+    //encapsulate in "user"
+    NSDictionary* post = [NSDictionary dictionaryWithObjectsAndKeys:
+                          userPost, @"user",
+                          nil];
+    
+    NSError *jsonError = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:post options:0 error:&jsonError];
+    NSString *json = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    NSURL* url = [self makeUrlFor:@"users"];
+    NSData* contents;
+    NSHTTPURLResponse* response = [self doRequestTo:url method:@"POST" input:json output:&contents cookie:nil];
+    BOOL didSucceed = YES;
+    //check response code
+    if ([response statusCode] != 201)
+    {
+        didSucceed = NO;
+        NSLog(@"Couldn't register user.");
+        NSString* responded = [[NSString alloc] initWithData:contents encoding:NSUTF8StringEncoding];
+        NSLog(@"Responded: %@", responded);
+        //interpret json response to set error
+        NSError *jsonError = nil;
+        NSDictionary* jsonContents = [NSJSONSerialization JSONObjectWithData:contents options:0 error:&jsonError];
+        *error = [NSString stringWithFormat:@"%@", [jsonContents valueForKey:@"error"]];
+    }
+    return didSucceed;
+}
+
+- (void) flushDataAndBlock {
+    [[CSSensorStore sharedSensorStore] forceDataFlushAndBlock];
+}
+
+///Creates the url using CommonSense.plist
+- (NSURL*) makeUrlFor:(const NSString*) action
+{
+    return [self makeUrlFor:action append:@""];
+}
+
+- (NSURL*) makeUrlFor:(const NSString*) action append:(NSString*) appendix
+{
+    NSString* url = [NSString stringWithFormat: @"%@/%@%@%@",
+                     kUrlBaseURLStaging,
+                     action,
+                     kUrlJsonSuffix,
+                     appendix];
+    
+    return [NSURL URLWithString:url];
+}
+
+- (NSHTTPURLResponse*) doRequestTo:(NSURL *)url method:(NSString*)method input:(NSString*)input output:(NSData**)output cookie:(NSString*) cookie {
+    NSError* error;
+    return [self doRequestTo:url method:method input:input output:output cookie:cookie error:&error];
+}
+
+- (NSHTTPURLResponse*) doRequestTo:(NSURL *)url method:(NSString*)method input:(NSString*)input output:(NSData**)output cookie:(NSString*) cookie error:(NSError **) error
+{
+    NSMutableURLRequest* urlRequest = [NSMutableURLRequest requestWithURL:url
+                                                              cachePolicy:NSURLRequestReloadIgnoringCacheData
+                                                          timeoutInterval:30];
+    //set method method
+    [urlRequest setHTTPMethod:method];
+    
+    //Cookie
+    if (cookie != nil)
+        [urlRequest setValue:cookie forHTTPHeaderField:@"cookie"];
+    if (self.applicationKey != nil)
+        [urlRequest setValue:self.applicationKey forHTTPHeaderField:@"APPLICATION-KEY"];
+    //Accept compressed response
+    [urlRequest setValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
+    
+    if (input != nil)
+    {
+        //Talking JSON
+        [urlRequest setValue:@"application/json" forHTTPHeaderField:@"content-type"];
+        const char* bytes = [input UTF8String];
+        NSData * body = [NSData dataWithBytes:bytes length: strlen(bytes)];
+        //compress the body
+        [urlRequest setValue:@"gzip" forHTTPHeaderField:@"Content-Encoding"];
+        [urlRequest setHTTPBody:[body gzippedData]];
+        //[urlRequest setHTTPBody:body];
+    }
+    
+    //connect
+    NSHTTPURLResponse* response=nil;
+    NSData* responseData;
+    
+    //Synchronous request
+    responseData = [NSURLConnection sendSynchronousRequest:urlRequest
+                                         returningResponse:&response
+                                                     error:error];
+    
+    //don't handle errors in the request, just log them
+    if (*error != nil) {
+        NSLog(@"Error during request \'%@\': %@",	[urlRequest description] ,	*error);
+        NSLog(@"Error description: \'%@\'.", [*error description] );
+        NSLog(@"Error userInfo: \'%@\'.", [*error userInfo] );
+        NSLog(@"Error failure reason: \'%@\'.", [*error localizedFailureReason] );
+        NSLog(@"Error recovery options reason: \'%@\'.", [*error localizedRecoveryOptions] );
+        NSLog(@"Error recovery suggestion: \'%@\'.", [*error localizedRecoverySuggestion] );
+    }
+    
+    //log response
+    if (response) {
+        NSLog(@"%@ \"%@\" responded with status code %ld", method, url, (long)[response statusCode]);
+        if (response.statusCode < 200 || response.statusCode >= 300) {
+            NSLog(@"Sent: %@", input);
+            NSLog(@"Received: %@", [[NSString alloc] initWithBytes:responseData.bytes length:responseData.length encoding:NSUTF8StringEncoding]);
+        }
+    }
+    
+    if (output != nil)
+    {
+        *output = responseData;
+    }
+    
+    return response;
 }
 
 @end
